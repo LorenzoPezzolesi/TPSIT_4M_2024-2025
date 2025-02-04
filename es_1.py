e5032a7a23a1f96e0f0f754382f43c93a1bd4336
pezzolesi <lorenzo.pezzolesi@studenti.isissgobetti.it>
2025-02-04 15:25:19 +0100

    es 1

--- es_1.py
+++ es_1.py
@@ -1,35 +1,35 @@
 import threading
 
-# Variabili globali per memorizzare i risultati
-result1 = 0
-result2 = 0
+# Variabili globali 
+resultato_1 = 0
+risultato_2 = 0
 
-# Funzione per calcolare 5 + 3
-def calculate_multiplication():
+
+def calcola_somma():
     global result1
     result1 = 5 + 3 
-    print(f"Risultato della moltiplicazione (3 * 4): {result1}")
+    print(f"Risultato della somma (5 + 3): {result1}")
 
-# Funzione per calcolare 4 - 2
-def calculate_subtraction():
-    global result2
-    result2 = 4 - 2
-    print(f"Risultato della sottrazione (5 - 2): {result2}")
 
-# Funzione principale
+def calcola_sottrazzione():
+    global risultato_2
+    risultato_2 = 4 - 2
+    print(f"Risultato della sottrazione (4 - 2): {risultato_2}")
+
+
 if __name__ == "__main__":
-    # Creazione dei thread
-    thread1 = threading.Thread(target=calculate_multiplication)
-    thread2 = threading.Thread(target=calculate_subtraction)
+    
+    thread_1 = threading.Thread(target=calculate_multiplication)
+    thread_2 = threading.Thread(target=calculate_subtraction)
 
-    # Avvio dei thread
-    thread1.start()
-    thread2.start()
+    
+    thread_1.start()
+    thread_2.start()
 
-    # Attesa del completamento dei thread
-    thread1.join()
-    thread2.join()
+    
+    thread_1.join()
+    thread_2.join()
 
     # Calcolo finale
-    final_result = result1 * result2
-    print(f"Risultato finale (5 + 3)  * (4 - 2): {final_result}")
+    risultato_finale = resultato_1 * risultato_2
+    print(f"Risultato finale (5 + 3)  * (4 - 2): {risultato_finale}")
