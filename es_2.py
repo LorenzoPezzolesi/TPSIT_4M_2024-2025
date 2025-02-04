159247087e4cebf74dfcb432a57e3e5d32d118cf
pezzolesi <lorenzo.pezzolesi@studenti.isissgobetti.it>
2025-02-04 15:49:55 +0100

    x

--- es_2.py
+++ es_2.py
@@ -1,2 +1,15 @@
+#Serie di fibonacci
+
 import threading
+
+#Variabili
+f_0 = 1
+f_1 = 1
+
+def calcola_somma():
+    f_n = f_0 + f_1 
+    print(f"Risultato della somma (5 + 3): {risultato_1}")
+
+
+
 
